87e0bd584d5df2eaf1d7207f616f31696a552eb2
Atahan Uysaler <atahan.uysaler@ftdbilisim.com>
2025-01-24 15:43:32 +0300

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Database setup
 const db = new sqlite3.Database('theatre.db');
